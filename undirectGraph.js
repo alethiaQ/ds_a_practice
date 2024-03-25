3c7b96f10d8a3f6290ca1184f65745f040bf3bdf
Alethia Quintero <alethiaq22@gmail.com>
2024-03-24 23:35:59 -0400

    hasPath function

--- undirectGraph.js
+++ undirectGraph.js
@@ -1,12 +1,11 @@
 const edges = [['i', 'j'], ['k', 'i'], ['m', 'k'], ['k', 'l'], ['o', 'n']];
 
-
 function undirectedGraph(edges, src, dest) {
     // turn edges into adjaceny list 
     // check path with dfs/bfs
     // make sure to mark as seen
     const graph = getGraph(edges);
-    console.log(graph);
+    return hasPath(graph, src, dest);
 }
 // takes in edges, returns an adjacency list, graph = {i: []..}
 function getGraph(edges) {
@@ -19,6 +18,15 @@ function getGraph(edges) {
         graph[b].push(a);
     }
     return graph;
+}
+
+function hasPath(graph, src, dest) {
+    if (src == dest) return true; 
+    console.log(graph[src]);
+    for (let node in graph[src]) {
+        hasPath(graph, node, dest);
+    }
+    return false;
 }
 
 undirectedGraph(edges, 'i', 'o');
