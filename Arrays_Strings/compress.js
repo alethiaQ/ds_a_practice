651226c29a2a27fe20551b0fb59a06f11b7a3f96
Alethia Quintero <alethiaq22@gmail.com>
2024-04-01 22:06:52 -0400

    compress 2 pointer complete

--- Arrays_Strings/compress.js
+++ Arrays_Strings/compress.js
@@ -1,22 +1,13 @@
 // Write a function, compress, that takes in a string as an argument. The function should return a compressed version of the string where consecutive occurrences of the same characters are compressed into the number of occurrences followed by the character. Single character occurrences should not be changed.
 
 const compress = (str) => {
-    
+    const ans = [];
     let i = 0; 
-    let j = 0; 
-    let ans = [];
-
-    while (j < str.length) {
+    for (let j = 0; j < str.length + 1; j++) {
         const count = j - i;
-        if (j == str.length - 1) {
-            ans.push(count + str[i]);
-            j += 1;
-        } else if (str[i] == str[j]) {
-            j += 1;
-         } else {
+        if (!(str[j] == str[i])) {
             count == 1 ? ans.push(str[i]) : ans.push(count + str[i]);
             i = j;
-            j += 1;
         }
     }
     console.log(ans.join(''));
