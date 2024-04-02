3e7142e49518cec4be9069fb161c6d577e0644c7
Alethia Quintero <alethiaq22@gmail.com>
2024-04-01 22:30:15 -0400

    anagrams edge case fix

--- Arrays_Strings/anagrams.js
+++ Arrays_Strings/anagrams.js
@@ -1,13 +1,14 @@
 // Write a function, anagrams, that takes in two strings as arguments. The function should return a boolean indicating whether or not the strings are anagrams. Anagrams are strings that contain the same characters, but in any order.
 
 const anagrams = (s1, s2) => {
-    let ans = true;
     if (!(s1.length == s2.length)) return false;
     let s1s = s1.split('').sort();
     let s2s = s2.split('').sort();
-    s2s === s1s ? ans = true : ans;
-    return ans;
+    for (let i = 0; i < s1.length; i++) {
+      if (s1s[i] != s2s[i]) return false
+    }
+    return true;
 }
 
-const answer = anagrams('elbw', 'below');
+const answer = anagrams('cats', 'tocs');
 console.log(answer);
