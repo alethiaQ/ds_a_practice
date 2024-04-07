12d0e089e0add681f48eb863b44e0fa2c7c3fe17
Alethia Quintero <alethiaq22@gmail.com>
2024-04-06 21:45:57 -0400

    merge list complete

--- Linked_Lists/mergeLists.js
+++ Linked_Lists/mergeLists.js
@@ -1,12 +1,43 @@
 // Write a function, mergeLists, that takes in the head of two sorted linked lists as arguments. The function should merge the two lists together into single sorted linked list. The function should return the head of the merged linked list.
 // Do this in-place, by mutating the original Nodes.
 // You may assume that both input lists are non-empty and contain increasing sorted numbers.
-import Node from "./LinkedList";
+
   
-  const mergeLists = (head1, head2) => {
-      // todo
-      return head1;
-  };
+const mergeLists = (head1, head2) => {
+    let dummyHead = new Node(null);
+    let tail = head1;
+    let current1 = head1;
+    let current2 = head2;
+    while (current1 != null && current2 != null) {
+        if (current1.val < current2.val) {
+          tail.next = current1;
+          current1 = current1.next;
+        } else {
+          tail.next = current1;
+          current2 = current2.next
+        }
+      tail = tail.next;
+    }
+    if (current1 != null) tail.next = current1;
+    if (current2 != null) tail.next = current2;
+    return dummyHead;
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const a = new Node(5);
 const b = new Node(7);
@@ -32,6 +63,3 @@ s.next = t;
 
 console.log(mergeLists(a, q));
 // 5 -> 6 -> 7 -> 8 -> 9 -> 10 -> 12 -> 20 -> 25 -> 28 
-module.exports = {
-    mergeLists,
-  };
