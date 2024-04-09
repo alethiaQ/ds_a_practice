57e8cc7e8e473da303c4b192f01e2eb741c95e69
Alethia Quintero <alethiaq22@gmail.com>
2024-04-08 21:37:20 -0400

    dft recursive

--- BinaryTree/depthFirstValues.js
+++ BinaryTree/depthFirstValues.js
@@ -7,21 +7,34 @@ class Node {
 }
 // Write a function, depthFirstValues, that takes in the root of a binary tree. The function should return an array containing all values of the tree in depth-first order.
 
+// const depthFirstValues = (root) => {
+//     let stack = [root];
+//     const ans = [];
+//     while (stack.length > 0) {
+//         let node = stack.pop();
+//         ans.push(node.val);
+//         if (node.left != null) {
+//             stack.push(node.left);
+//         }
+//         if (node.right != null) {
+//             stack.push(node.right)
+//         }
+//     }
+//     return ans;
+// };
+
 const depthFirstValues = (root) => {
-    let stack = [root];
-    const ans = [];
-    while (stack.length > 0) {
-        let node = stack.pop();
-        ans.push(node.val);
-        if (node.left != null) {
-            stack.push(node.left);
-        }
-        if (node.right != null) {
-            stack.push(node.right)
-        }
-    }
-    return ans;
+    if (root == null) return [];
+    let leftValues = depthFirstValues(root.left);
+    let rightValues = depthFirstValues(root.right);
+
+    return [root.val, ...leftValues, ...rightValues]
 };
+
+
+
+
+
   
 const a = new Node('a');
 const b = new Node('b');
